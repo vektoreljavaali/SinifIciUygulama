4b27e378b03f81d27fc086fd05376b9a6304ba92
vektorel <vektorel@lab2-ogr>
2018-09-22 20:10:04 +0200

    ders sonu

--- src/com/vektorel/Beans/indexBean.java
+++ src/com/vektorel/Beans/indexBean.java
@@ -1,5 +1,52 @@
 package com.vektorel.Beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.vektorel.Models.tbldefault;
+
+@SuppressWarnings("deprecation")
+@ManagedBean(name="AnasayfaBean")
+@SessionScoped
 public class indexBean {
 
+	private String ekranyazisi = "Ýlk JSF";
+
+	private List<tbldefault> liste = new ArrayList<>();
+	
+	public indexBean() {
+		tbldefault tb;
+		
+		for (Long i = 0L; i < 10; i++) {
+			tb = new tbldefault();
+			tb.setId(i);
+			tb.setAd("Kiþi-"+i);
+			liste.add(tb);			
+		}
+		
+	}
+	
+	
+	public String getEkranyazisi() {
+		return ekranyazisi;
+	}
+
+	public void setEkranyazisi(String ekranyazisi) {
+		this.ekranyazisi = ekranyazisi;
+	}
+
+	public List<tbldefault> getListe() {
+		return liste;
+	}
+
+	public void setListe(List<tbldefault> liste) {
+		this.liste = liste;
+	}
+	
+	
+	
+	
 }
--- src/com/vektorel/Models/tbldefault.java
+++ src/com/vektorel/Models/tbldefault.java
@@ -2,4 +2,26 @@ package com.vektorel.Models;
 
 public class tbldefault {
 
+	private Long id;
+	
+	private String ad;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAd() {
+		return ad;
+	}
+
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	
+	
+	
 }
