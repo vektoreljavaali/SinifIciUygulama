951fda93ac0ed80927fc6ea83378b8adf009f266
vektorel <vektorel@lab2-ogr>
2018-09-23 20:04:48 +0200

    Admin Panel eklendi AnaSayfaya tema olarak okullar eklendi.

--- src/com/vektorel/Beans/indexBean.java
+++ src/com/vektorel/Beans/indexBean.java
@@ -7,6 +7,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import com.vektorel.Models.tbldefault;
+import com.vektorel.Models.tblokullar;
 
 @SuppressWarnings("deprecation")
 @ManagedBean(name="AnasayfaBean")
@@ -16,20 +17,58 @@ public class indexBean {
 	private String ekranyazisi = "Ýlk JSF";
 
 	private List<tbldefault> liste = new ArrayList<>();
+	private List<tblokullar> okullistesi = new ArrayList<>();
 	
-	public indexBean() {
-		tbldefault tb;
-		
-		for (Long i = 0L; i < 10; i++) {
-			tb = new tbldefault();
-			tb.setId(i);
-			tb.setAd("Kiþi-"+i);
-			liste.add(tb);			
-		}
-		
+	private tblokullar okulgiris = new tblokullar();
+	
+	public void okulekle() {
+		okullistesi.add(okulgiris);
 	}
 	
 	
+	public indexBean() {
+		tblokullar okul;
+		
+		okul = new tblokullar();
+		okul.setId(1L);
+		okul.setOkuladi("A. Kalpler Anaokulu");
+		okul.setOkuladresi("Öveçler Dikmen");
+		okul.setOkulwebsiteadresi("https://okul.com.tr/anaokulu/ankara/ozel-asil-kalpler-anaokulu-82");
+		okul.setResimurl("https://cdn.okul.com.tr/schools/82/360x310/ozel-asil-kalpler-anaokulu-1.jpg");
+		okullistesi.add(okul);
+	}
+	
+	
+	
+	
+	public tblokullar getOkulgiris() {
+		return okulgiris;
+	}
+
+
+
+
+	public void setOkulgiris(tblokullar okulgiris) {
+		this.okulgiris = okulgiris;
+	}
+
+
+
+
+	public List<tblokullar> getOkullistesi() {
+		return okullistesi;
+	}
+
+
+
+
+	public void setOkullistesi(List<tblokullar> okullistesi) {
+		this.okullistesi = okullistesi;
+	}
+
+
+
+
 	public String getEkranyazisi() {
 		return ekranyazisi;
 	}
---
+++ src/com/vektorel/Models/tblokullar.java
@@ -0,0 +1,46 @@
+package com.vektorel.Models;
+
+public class tblokullar {
+
+	private Long id;
+	private String okuladi;
+	private String resimurl;
+	private String okuladresi;
+	private String okulwebsiteadresi;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOkuladi() {
+		return okuladi;
+	}
+	public void setOkuladi(String okuladi) {
+		this.okuladi = okuladi;
+	}
+	public String getResimurl() {
+		return resimurl;
+	}
+	public void setResimurl(String resimurl) {
+		this.resimurl = resimurl;
+	}
+	public String getOkuladresi() {
+		return okuladresi;
+	}
+	public void setOkuladresi(String okuladresi) {
+		this.okuladresi = okuladresi;
+	}
+	public String getOkulwebsiteadresi() {
+		return okulwebsiteadresi;
+	}
+	public void setOkulwebsiteadresi(String okulwebsiteadresi) {
+		this.okulwebsiteadresi = okulwebsiteadresi;
+	}
+	
+	
+	
+	
+}
